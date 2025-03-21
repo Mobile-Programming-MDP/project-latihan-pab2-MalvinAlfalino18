6d1f220673fec0f889a72137f0c43a7a7385d57b
Kolin-ko <kkolinn4457@gmail.com>
2025-03-21 17:31:23 +0700

    malvin2125240038
    create project

---
+++ karyawan/lib/main.dart
@@ -0,0 +1,84 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:karyawan/models/karyawan.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Daftar Karyawan",
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  //Karena Future bearti Asyncrounus
+  Future<List<Karyawan>> _readJsonData() async {
+    //Untuk Menampung String dari JSON KARYAWAN
+    String response = await rootBundle.loadString("assets/karyawan.json");
+
+    //decode -> Mengubah String JSON menjadi List<dynamic>
+    final List<dynamic> data = json.decode(response);
+    return data.map((json) => Karyawan.fromJson(json)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Daftar Karyawan"),
+      ),
+      body: FutureBuilder<List<Karyawan>>(
+        future: _readJsonData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      snapshot.data![index].nama,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Umur : ${snapshot.data![index].umur} tahun"),
+                        Text(
+                            "Alamat : ${snapshot.data![index].alamat.jalan}, ${snapshot.data![index].alamat.kota}, ${snapshot.data![index].alamat.provinsi},"),
+                        // if(snapshot.data![index].hobi.length > 1){
+                        //   Text("Hobby : ${snapshot.data![index].hobi[0]}"),
+                        // }
+                        Text("Hobby : ${snapshot.data![index].hobi[0]}"),
+                      ],
+                    ),
+                  );
+                });
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text("Error: ${snapshot.error}"),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(), //effect loading
+          );
+        },
+      ),
+    );
+  }
+}
